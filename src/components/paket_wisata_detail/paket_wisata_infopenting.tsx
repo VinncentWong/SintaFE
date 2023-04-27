19384730c1696967e21bc8709e62267085589872
VinncentWong <vinncentwong2@gmail.com>
2023-04-27 16:33:28 +0700

    [ADD] paket wisata info penting

---
+++ src/components/paket_wisata_detail/paket_wisata_infopenting.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import PaketWisataTitle from "./paket_wisata_title";
+
+const PaketWisataInfoPenting = () => {
+    return(
+        <Box>
+            
+        </Box>
+    )
+};
+
+export default PaketWisataInfoPenting;
