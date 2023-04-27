cb1f985cdef9414ce4071509cf383d3c3bc34e68
VinncentWong <vinncentwong2@gmail.com>
2023-04-27 16:33:56 +0700

    [ADD] google icon

---
+++ src/icon/google_icon.tsx
@@ -0,0 +1,8 @@
+import { Icon } from "@chakra-ui/react";
+import { FcGoogle } from "react-icons/fc";
+
+const GoogleIcon = () => {
+    return <Icon as={FcGoogle}/>
+};
+
+export default GoogleIcon;
