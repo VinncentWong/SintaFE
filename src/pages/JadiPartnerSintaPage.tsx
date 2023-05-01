f0e8551db1e59d1a71eb939d6686a58f783947b6
VinncentWong <vinncentwong2@gmail.com>
2023-05-01 17:45:05 +0700

    [REFACTOR] some attributes on navbar

--- src/pages/JadiPartnerSintaPage.tsx
+++ src/pages/JadiPartnerSintaPage.tsx
@@ -2,11 +2,12 @@ import { Box } from "@chakra-ui/react";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import Navbar from "../components/navbar";
+import auth_util from "../util/auth_util";
 
 const JadiPartnerSintaPage = () => {
     return(
         <Box>
-            <Navbar isAuthenticated={false} type="other"/>
+            <Navbar isAuthenticated={auth_util()} type="other"/>
             <Header type="jadipartner"/>
             <Footer/>
         </Box>
