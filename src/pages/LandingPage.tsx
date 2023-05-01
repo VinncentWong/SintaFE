56c623c954cd4b51626caceeab9e5fcc53e7c5b2
VinncentWong <vinncentwong2@gmail.com>
2023-05-01 17:45:44 +0700

    [REMOVE] card raw data and instead extract data from json file

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -6,49 +6,25 @@ import Header from "../components/header";
 import Navbar from "../components/navbar";
 import WhySinta from "../components/whysinta/whysinta";
 import LandingContext from "../context/LandingContext";
-import sample from "../images/sample/destination1.png";
-
-const data: DestinationCardProps = {
-    destinationName : "Paket Wisata Bali Full Trip",
-    destinationCity: "Batu",
-    destinationPrice: 192000,
-    destinationProvince: "Jawa Timur",
-    imageLink: sample,
-    typeDestination: "1D Trip"
-}
-
-const arr = [
-    <DestinationCard 
-    destinationCity={data.destinationCity}
-    destinationName={data.destinationName}
-    destinationPrice={data.destinationPrice}
-    destinationProvince={data.destinationProvince}
-    imageLink={data.imageLink}
-    typeDestination={data.typeDestination}/>,
-    <DestinationCard 
-    destinationCity={data.destinationCity}
-    destinationName={data.destinationName}
-    destinationPrice={data.destinationPrice}
-    destinationProvince={data.destinationProvince}
-    imageLink={data.imageLink}
-    typeDestination={data.typeDestination}/>,
-    <DestinationCard 
-    destinationCity={data.destinationCity}
-    destinationName={data.destinationName}
-    destinationPrice={data.destinationPrice}
-    destinationProvince={data.destinationProvince}
-    imageLink={data.imageLink}
-    typeDestination={data.typeDestination}/>,
-    <DestinationCard 
-    destinationCity={data.destinationCity}
-    destinationName={data.destinationName}
-    destinationPrice={data.destinationPrice}
-    destinationProvince={data.destinationProvince}
-    imageLink={data.imageLink}
-    typeDestination={data.typeDestination}/>
-]
+import auth_util from "../util/auth_util";
+import cardData from "../data/card_paketwisata.json";
+import gambar from "../images/sample/destination1.png";
+import { PaketWisata } from "../response/paket_wisata";
 
 const LandingPage = () => {
+
+    const arrDestinationCard = cardData.paket_wisata.map((d: PaketWisata) => {
+        return <DestinationCard
+        destinationCity={d.kota}
+        destinationName={d.nama}
+        destinationPrice={d.harga}
+        destinationProvince={d.provinsi}
+        imageLink={gambar}
+        typeDestination={d.lama_paket_wisata + " Trip"}
+        width="33%"
+        height="23%"
+        />
+    });
 
     const [position, setPosition] = useState<number>(0);
     window.addEventListener("scroll", (e: Event) => {
@@ -59,24 +35,24 @@ const LandingPage = () => {
         <LandingContext.Provider value={{
             position: position,
         }}>
-            <Navbar isAuthenticated={false} type="landing"/>
+            <Navbar isAuthenticated={auth_util()} type="landing"/>
             <Header type="landing"/>
             <DestinationGroup 
             title="Pilihan destinasi menarik di dalam negeri!"
             subtitle="Yuk, temukan destinasi favoritmu! Ada banyak pilihan paket wisata menantimu 🤗"
-            data={arr}/>
+            data={arrDestinationCard}/>
             <DestinationGroup 
             title="Pilihan destinasi menarik di luar negeri!!"
             subtitle="Yuk, temukan destinasi favoritmu! Ada banyak pilihan paket wisata menantimu 🤗"
-            data={arr}/>
+            data={arrDestinationCard}/>
             <DestinationGroup 
             title="Pilihan destinasi menarik under 500k 😱"
             subtitle="Cocok buat kamu yang punya budget terbatas, tapi mau liburan yang seru!"
-            data={arr}/>
+            data={arrDestinationCard}/>
             <DestinationGroup 
             title="Include semuanya tinggal bawa badan"
             subtitle="Gaperlu repot mikirin biaya lainnya, cukup bawa badan and enjoy ur holiday 🤩"
-            data={arr}/>
+            data={arrDestinationCard}/>
             <WhySinta/>
             <Footer/>
         </LandingContext.Provider>
