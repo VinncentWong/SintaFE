2c261ac6c9d2668f0b25ad8ff26e8baad05e2e82
VinncentWong <vinncentwong2@gmail.com>
2023-05-15 10:40:28 +0700

    [ADD] new auth method

--- src/util/auth_util.ts
+++ src/util/auth_util.ts
@@ -1,7 +1,19 @@
+import { AgenTravel } from "../response/agen_travel";
+
 export const getJwtToken = () => {
     const jwt = localStorage.getItem("jwtToken");
     return jwt;
 };
+
+export const getAgenTravel = () => {
+    const agenTravel = JSON.parse(localStorage.getItem("agenTravel") as string) as AgenTravel;
+    return agenTravel;
+}
+
+export const getJwtAgenTravel = () => {
+    const jwtToken = localStorage.getItem("jwtTokenAgenTravel");
+    return jwtToken;
+}
 
 export default () => {
     if(localStorage.getItem("user")){
