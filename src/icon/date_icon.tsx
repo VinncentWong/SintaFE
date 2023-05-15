0def63d578dc8e6916a5f7be56ca028404483c61
VinncentWong <vinncentwong2@gmail.com>
2023-05-15 10:38:55 +0700

    [ADD] date icon

---
+++ src/icon/date_icon.tsx
@@ -0,0 +1,8 @@
+import { Icon } from "@chakra-ui/react"
+import {MdOutlineDateRange} from "react-icons/md";
+
+const DateIcon = () => {
+    return <Icon as={MdOutlineDateRange}/>
+}
+
+export default DateIcon;
