abe6cabcb07bcc2d1a0dfca5c9073d04f9ca710c
VinncentWong <vinncentwong2@gmail.com>
2023-05-15 10:37:22 +0700

    [REFACTOR] input component

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Input, Text } from "@chakra-ui/react";
+import { Box, BoxProps, Input, Text } from "@chakra-ui/react";
 import { fontFamily } from "../../style/font";
 import { ChangeEvent } from "react";
 
@@ -6,13 +6,62 @@ interface InputSintaProps{
     description: string,
     placeholder: string,
     notes: string,
-    value: string,
-    onChange: (e: ChangeEvent<HTMLInputElement>) => void,
+    value?: string,
+    onChange?: (e: ChangeEvent<HTMLInputElement>) => void,
     width: string,
-    type: React.HTMLInputTypeAttribute
+    type: React.HTMLInputTypeAttribute,
+    isDisable?: boolean,
+    additionalProps?: BoxProps,
+    optional?: boolean
 }
 
-const InputSinta = ({description, notes, onChange, placeholder, value, width, type}: InputSintaProps) => {
+const InputSinta = ({description, notes, onChange, placeholder, value, width, type, isDisable, additionalProps, optional}: InputSintaProps) => {
+    if(isDisable){
+        return (
+            <Box
+                    width={{
+                        "lg" : "100%"
+                    }}
+                    marginTop={{
+                        "lg" : "1rem"
+                    }}
+                    marginBottom={{
+                        "lg" : "1rem"
+                    }}
+                    {...additionalProps}>
+                        <Text
+                        fontFamily={fontFamily}
+                        fontWeight={500}
+                        fontSize="1rem"
+                        marginY={{
+                            "lg" : "0.5rem"
+                        }}
+                        color="#89939E">
+                            {description}
+                        </Text>
+                        <Input 
+                        isDisabled
+                        placeholder={placeholder}
+                        _placeholder={{
+                            "color" : "#717171"
+                        }}
+                        width={{
+                            "lg" : width
+                        }}
+                        marginBottom={{
+                            "lg" : "0.5rem"
+                        }}
+                        value={value}
+                        onChange={onChange}
+                        type={type}/>
+                        <Text
+                        fontFamily={fontFamily}
+                        fontWeight={400}
+                        fontSize="0.75rem"
+                        color="#89939E">{notes}</Text>
+                </Box>
+        );
+    }
     return (
         <Box
                 width={{
@@ -23,17 +72,31 @@ const InputSinta = ({description, notes, onChange, placeholder, value, width, ty
                 }}
                 marginBottom={{
                     "lg" : "1rem"
-                }}>
-                    <Text
-                    fontFamily={fontFamily}
-                    fontWeight={500}
-                    fontSize="1rem"
-                    marginY={{
-                        "lg" : "0.5rem"
-                    }}>
-                        {description}<span style={{
-                        "color" : "#E12C1F"
-                    }}>*</span></Text>
+                }}
+                {...additionalProps}>
+                    {
+                        optional == true ?
+                        <Text
+                        fontFamily={fontFamily}
+                        fontWeight={500}
+                        fontSize="1rem"
+                        marginY={{
+                            "lg" : "0.5rem"
+                        }}>
+                        {description}(optional)
+                        </Text>
+                    :
+                        <Text
+                        fontFamily={fontFamily}
+                        fontWeight={500}
+                        fontSize="1rem"
+                        marginY={{
+                            "lg" : "0.5rem"
+                        }}>
+                            {description}<span style={{
+                            "color" : "#E12C1F"
+                        }}>*</span></Text>
+                    }
                     <Input 
                     placeholder={placeholder}
                     _placeholder={{
